2811ab4f77537a70dbedc240ccd22c012ec2553b
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2020-07-23 05:22:14 +0100

    help

--- commands/7help.js
+++ commands/7help.js
@@ -8,6 +8,9 @@ module.exports = {
             !patch: To display latest patch notes (To come).
             !gear [type] [basestat] : To know if your SSR gear is good to awaken. 
             !gear info : Lets you know acceptable base stat values for the gear tool.
+            !friend set [yourcode] = Saves your code on a database.
+            !friend [username] = Search for this user and retrieves his friend code.
+            !friend all = An embed with all the friend codes.
             `,
     );
   },

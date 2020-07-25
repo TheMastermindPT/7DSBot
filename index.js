ea8cf7b83588980936518aa883a799a8e757ea1b
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2020-07-25 02:18:02 +0100

    v1.0.0

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
 
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
 http.createServer(app).listen(PORT, async () => {
   client.on('ready', async () => {
     const cloverDiscord = client.guilds.cache.find((guild) => guild.id === guildID);
@@ -94,29 +99,23 @@ http.createServer(app).listen(PORT, async () => {
 
     console.log('Bot is executing!');
   });
-
-  for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-  }
-
-  client.on('message', async (message) => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if (!client.commands.has(command)) return;
-    try {
-      client.commands.get(command).execute(message, args);
-    } catch (error) {
-	  console.error(error);
-	  message.reply('There was an error trying to execute that command!');
-    }
-  });
-
-  // Authenticates the bot with the token
-  client.login(token);
-
   console.log(`Express server listening on port ${PORT}`);
 });
+
+client.on('message', async (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (!client.commands.has(command)) return;
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('There was an error trying to execute that command!');
+  }
+});
+
+// Authenticates the bot with the token
+client.login(token);

c13a6ea4d5daf19163bea07e612fc95d56fff2f6
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2020-08-10 23:56:31 +0100

    fixed joke

--- commands/insult.js
+++ commands/insult.js
@@ -7,7 +7,7 @@ module.exports = {
   name: 'insult',
   description: 'List of commands for 7DSPatchBot',
   execute(message, args) {
-    const regex = /^(<@!\d{18}>$)/i;
+    const regex = /^<@\d{18}>$/i;
     if (regex.test(args[0])) {
       const joke = jokes[Math.floor(Math.random() * jokes.length)];
       message.channel.send(`Hey ${args[0]} ${joke}`);

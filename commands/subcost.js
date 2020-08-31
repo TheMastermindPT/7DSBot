78ccd7086e36742707875e32f3fba3be741422ad
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2020-08-31 17:56:39 +0100

    better cost

--- commands/subcost.js
+++ commands/subcost.js
@@ -6,44 +6,32 @@ module.exports = {
   execute(message, args) {
     try {
       if (args[0] === 'c' || args[0] === 'uc' || args[0] === 'r') {
-        let maxValue;
         let rarityCost;
-
-        switch (args[1]) {
-          case '3':
-            maxValue = 3;
-            break;
-          case '6':
-            maxValue = 6;
-            break;
-          case '4.5':
-            maxValue = 4.5;
-            break;
-          default:
-            message.channel.send('Insert a valid max value for the substat.');
-            break;
-        }
-        if (!maxValue) return;
 
         if (args[0] === 'c') rarityCost = 5000;
         if (args[0] === 'uc') rarityCost = 7000;
         if (args[0] === 'r') rarityCost = 10000;
 
-        const statCost = rarityCost / ((1 / 4) * (0.1 / maxValue));
+        const statCost3 = rarityCost / ((1 / 4) * (0.1 / 3));
+        const statCost45 = rarityCost / ((1 / 4) * (0.1 / 4.5));
+        const statCost6 = rarityCost / ((1 / 4) * (0.1 / 6));
 
         const gearCost = new Discord.MessageEmbed()
           .setColor('#821d01')
           .setTitle('**Average cost to get max roll**')
           .setThumbnail('https://imgur.com/Z49j86V.jpg')
           .addFields(
-            { name: '*Cost:*', value: `*${statCost}*` },
+            { name: '*Atk/Def/Hp:*', value: `*${statCost3}*` },
+            { name: '*C.chance/ C.Res /Lifesteal:*', value: `*${statCost45}*` },
+            { name: '*Pierce/ Res/ Cdmg/ C.Def / Recov.R / Regen.R: *', value: `*${statCost6}*` },
           )
           .setTimestamp();
         return message.channel.send(gearCost);
       }
-      message.channel.send('Insert a valid rarity type for gear. Ex: c/uc/r');
+      return message.channel.send('Insert a valid rarity type for gear. Ex: c/uc/r');
     } catch (err) {
       console.error(err);
     }
+    return 0;
   },
 };

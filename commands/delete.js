d2fdcb51b9e1ef7b0401449be6aeeaacb09be980
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2020-09-11 00:51:15 +0100

    start of Hawk & Ale

--- commands/delete.js
+++ commands/delete.js
@@ -1,4 +1,4 @@
-// Server Manager = 734092416092471386
+// Tavern Owner = 753762800303603792
 // Admins = 662888342555197451
 
 module.exports = {
@@ -6,7 +6,7 @@ module.exports = {
   description: 'Deletes messages from the channel from to 2 up to 100.',
   execute(message, args) {
     const amount = parseInt(args[0], 10) + 1;
-    if (message.member.roles.cache.has('734092416092471386')) {
+    if (message.member.roles.cache.has('734092416092471386') || message.member.roles.cache.has('753762800303603792')) {
       if (isNaN(amount)) {
         return message.reply('that doesn\'t seem to be a valid number.');
       } if (amount <= 1 || amount > 100) {

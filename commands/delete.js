eece5743b99e4687c08437bbabdc6f4f621ccf72
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2020-09-22 02:11:15 +0100

    delete

--- commands/delete.js
+++ commands/delete.js
@@ -1,12 +1,11 @@
 // Tavern Owner = 753762800303603792
-// Admins = 662888342555197451
 
 module.exports = {
   name: 'delete',
   description: 'Deletes messages from the channel from to 2 up to 100.',
   execute(message, args) {
     const amount = parseInt(args[0], 10) + 1;
-    if (message.member.roles.cache.has('734092416092471386') || message.member.roles.cache.has('753762800303603792')) {
+    if (message.member.roles.cache.has('753762800303603792')) {
       if (isNaN(amount)) {
         return message.reply('that doesn\'t seem to be a valid number.');
       } if (amount <= 1 || amount > 100) {
